cb04a0c6fbb20f14b32e065c18634a50078536ef
vincenzocorso <vincenzocorso99@gmail.com>
2018-11-12 19:59:18 +0100

    Add another version of es4.c

---
+++ lez14/es4b.c
@@ -0,0 +1,29 @@
+/*
+	Esercizio 4
+	Sia data una griglia di dimensione nxn con n fornito in input, e
+	si consideri di numerare in ordine crescente, e per riga, la parte
+	triangolare inferiore della matrice. Si scriva un programma in C
+	che visualizza il conteggio per come scritto in griglia, con i
+	numeri separati dalla ", ".
+*/
+#include <stdio.h>
+
+int main()
+{
+	// Prende in input un numero intero positivo.
+	unsigned int n;
+	printf("Inserisci un numero intero positivo: ");
+	scanf("%u", &n);
+	
+	/* Scorre le entrate della matrice in posizione i < j
+	   Definizione di matrice triangolare inferiore. */
+	for(unsigned int riga = 1; riga <= n; riga++)
+	{
+		for(unsigned int colonna = 1; colonna <= riga; colonna++)
+			printf("%4d, ", ((riga - 1) * riga) / 2 + colonna);
+		
+		printf("\n");
+	}
+	
+	return 0;
+}
